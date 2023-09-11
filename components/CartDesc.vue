4d0aaf671ac3f6a64ba6eaa3d7c358bd0ea34ea9
Ernesto Canquiz <fdsoil123@gmail.com>
2023-09-11 17:25:57 -0400

    feat: My first commit

---
+++ components/CartDesc.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+  product: { type: Object },
+});
+</script>
+<template>
+  <div class="">
+    <div>{{ product.name }} - {{ product.presentation }}</div>
+    <div>{{product.saleType}} - <span class="text-green-500">${{product.price}}</span></div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
+
