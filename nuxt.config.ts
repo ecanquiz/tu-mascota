b78039babdbbbb8668a78c12d3515f2108c42962
Ernesto Canquiz <fdsoil123@gmail.com>
2023-09-11 18:22:46 -0400

    feat: Deploy config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  //ssr: true,
+  app: {
+    baseURL: '/tu-mascota/',
+  },
   modules: [
     '@nuxtjs/tailwindcss',
     '@pinia/nuxt',
