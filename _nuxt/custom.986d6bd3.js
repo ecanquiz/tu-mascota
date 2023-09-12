2991b03bee9ea2ef731183389b544b670e86b99d
Ernesto Canquiz <caribestic123@gmail.com>
2023-09-12 10:01:18 -0400

    Updates

---
+++ _nuxt/custom.986d6bd3.js
@@ -0,0 +1,1 @@
+import{_ as s,c as t,a,J as o,o as c}from"./entry.6b1edacd.js";const r={},n={class:"mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5"};function l(e,p){return c(),t("div",null,[a("div",n,[o(e.$slots,"default")])])}const _=s(r,[["render",l]]);export{_ as default};
