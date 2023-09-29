2f53f24249b4af8d34cbca1c6684ef87ff94ed08
Ernesto Canquiz <fdsoil123@gmail.com>
2023-09-29 10:53:36 -0400

    Updates

--- _nuxt/default.ade27697.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,l as r,G as n,k as o}from"./entry.8c812fd1.js";const s={};function a(e,c){return o(),r("div",null,[n(e.$slots,"default")])}const _=t(s,[["render",a]]);export{_ as default};
